ee042b8f397eab15d9fdf4bfe8ff50c87e6a9511
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2020-03-07 16:46:58 +0200

    Update docker_install_with_user.sh

--- docker_install_with_user.sh
+++ docker_install_with_user.sh
@@ -1,49 +1,14 @@
 #!/bin/bash
-source <(curl -s https://raw.githubusercontent.com/MaksymSemenykhin/bash_scripts/master/output.sh)
-dockerFodler="/docker"
+
+curl -s https://raw.githubusercontent.com/MaksymSemenykhin/docker_install_bash_script/master/docker_install.sh -H "Cache-Control: no-cache" | bash 
+
 dockeruser="ubuntu"
-
-print_title 'SET UP THE REPOSITORY'
-
-print_info 'Updating the apt package index'
-sudo apt-get update -qq
-
-print_info 'Installing packages to allow apt to use a repository over HTTPS'
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -yqq
-
-print_info 'Addiing Docker’s official GPG key'
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-
-print_info 'Adding stable repository'
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-print_title 'Updating the apt package index'
-
-print_info 'Update the apt package index'
-sudo apt-get update
-
-print_info 'Installing the latest version of Docker Engine - Community and containerd'
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
-
-print_title 'Folders prepare'
-mkdir $dockerFodler -p
+dockerFolder="/docker"
 
 print_title "Set $dockeruser user as docker folder owner"
-chown $dockeruser:$dockeruser /docker -R
+chown $dockeruser:$dockeruser $dockerFolder -R
 
 print_title "Add $dockeruser user to docker group"
 sudo groupadd docker
 sudo usermod -aG docker $dockeruser
 
-print_title 'Result:'
-docker -v
-docker-compose -v
