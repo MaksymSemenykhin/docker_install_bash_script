1b6981bc141f7195142773e8b451d8ff65907e27
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2020-03-07 08:53:00 +0200

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/MaksymSemenykhin/bash_scripts/master/output.sh)
+
+print_title 'SET UP THE REPOSITORY'
+
+print_info 'Updating the apt package index'
+sudo apt-get update -qq
+
+print_info 'Installing packages to allow apt to use a repository over HTTPS'
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -yqq
+
+print_info 'Addiing Docker’s official GPG key'
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+print_info 'Adding stable repository'
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+print_title 'Updating the apt package index'
+
+print_info 'Update the apt package index'
+sudo apt-get update
+
+print_info 'Installing the latest version of Docker Engine - Community and containerd'
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+print_title 'Result:'
+docker -v
